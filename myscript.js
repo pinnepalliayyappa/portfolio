e536e6b57056ba87d6ea6bb2632e07cc3a53b50b
pinnepalliayyappa <68156666+pinnepalliayyappa@users.noreply.github.com>
2022-03-03 17:55:01 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,15 +1,16 @@
 var myImage=document.getElementById("image")
 var count=0
-function changeImage() {
-  let mySrc = myImage.getAttribute('src');
-  var photos=['2.jpg']
-  if (count==photos.length) {
-    count=0
-  }
-  else {
-     myImage.setAttribute('src',photos[count]);
-     count +=1
-  }
-}
+myImage.setAttribute('src','2.jpg');
+//function changeImage() {
+  //let mySrc = myImage.getAttribute('src');
+  //var photos=['2.jpg']
+  //if (count==photos.length) {
+    //count=0
+  //}
+  //else {
+     //myImage.setAttribute('src',photos[count]);
+     //count +=1
+  //}
+//}
 
-setInterval("changeImage()",1500)
+//setInterval("changeImage()",1500)
